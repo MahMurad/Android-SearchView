db0d796ed71196ac1b4fcf0b83d5d6e59a20808c
MahMurad <52890377+MahMurad@users.noreply.github.com>
2019-08-19 19:23:33 +0600

    Commit 2...

--- app/src/main/java/com/example/app125listview/MainActivity.java
+++ app/src/main/java/com/example/app125listview/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
         listView.setOnItemLongClickListener(new OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(MainActivity.this,position+1 +"."+ listArray[position] +"XoXo",Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this,position+1 +"."+ listArray[position] +" (X0X0)",Toast.LENGTH_SHORT).show();
                 return false;
             }
         });
