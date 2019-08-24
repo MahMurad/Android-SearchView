86f8a5c738f0ff57fda3ca02e8e2df26a2e89737
MahMurad <52890377+MahMurad@users.noreply.github.com>
2019-08-25 00:25:11 +0600

    Filtering feature added...

--- app/src/main/java/com/example/app125listview/MainActivity.java
+++ app/src/main/java/com/example/app125listview/MainActivity.java
@@ -3,34 +3,51 @@ package com.example.app125listview;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemLongClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SearchView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
      private ListView listView;
      private String[] countryNames;
-    ArrayAdapter<String> adapter;
+     private ArrayAdapter<String> adapter;
+     private SearchView searchView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        listView = findViewById(R.id.ListViewId);
+        listView = (ListView) findViewById(R.id.ListViewId);
+        searchView = (SearchView) findViewById(R.id.searchViewId);
 
         countryNames = getResources().getStringArray(R.array.listItem);
-        adapter = new ArrayAdapter(MainActivity.this,R.layout.adapter_layout,R.id.textView1Id,countryNames);
-        listView.setAdapter(adapter);
-        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
 
+        adapter = new ArrayAdapter<String>(MainActivity.this,R.layout.adapter_layout,R.id.textView1Id,countryNames);
+        listView.setAdapter(adapter);
+        listView.setOnItemLongClickListener(new OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
                 Toast.makeText(MainActivity.this,position+1+"."+countryNames[position],Toast.LENGTH_SHORT).show();
                 return false;
             }
         });
-    }
 
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                adapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+
+    }
 }
